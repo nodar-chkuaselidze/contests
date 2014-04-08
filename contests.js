f7e2fdfba4c603fd3e70bc0e3c67c9a6570ee051
Nodar Chkuaselidze <nodar.chkuaselidze@gmail.com>
2014-04-08 10:38:27 +0400

    test some env vals

--- contests.js
+++ contests.js
@@ -1,2 +1,14 @@
 'use strict';
-console.log('Hello contest');
+
+var program = require('./package.json'),
+    debug = require('debug')('contests.js'),
+    fs    = require('fs'),
+    shell = require('commander');
+
+
+shell
+  .version(program.version)
+  .parse(process.argv);
+
+console.log(process.env['USERPORFILE']);
+console.log(process.env['HOME']);
