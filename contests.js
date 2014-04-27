89b3dae25d2e97213d2491aa51a8c4e72fc5f073
Nodari Chkuaselidze <nodar.chkuaselidze@gmail.com>
2014-04-27 21:52:51 +0400

    Add engines class

--- contests.js
+++ contests.js
@@ -14,8 +14,24 @@ global.cacher = new Cache(program.name);
 cacher.createCacheDir();
 
 var args  = require('commander'),
-    debug = require('debug')('contests');
+    debug = require('debug')('contests'),
+    Engines = require('./lib/Engines'),
+    engines = new Engines('./engines');
 
 
 args.version(program.version);
+args.command('list')
+    .description('List of contests')
+    .action(function (cmd, options) {
+      var padd = '  ';
+      padd += padd;
+
+      console.log('  Engines List:');
+      engines.getList().forEach(function (engine) {
+        console.log(padd + engine);
+      });
+    });
+
+args.parse(process.argv);
+
 debug('Version of ' + program.name + ' is ' + program.version);
