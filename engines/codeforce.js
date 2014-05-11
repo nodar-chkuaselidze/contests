d566c57d805d71f94ac2c209a4e066efa1a67785
Nodari Chkuaselidze <nodar.chkuaselidze@gmail.com>
2014-05-11 13:28:02 +0400

    Implementing codeForce with promises

--- engines/codeforce.js
+++ engines/codeforce.js
@@ -7,12 +7,36 @@ function CF(problem, file, cacher, confs) {
   this.file    = file;
   this.cacher  = cacher;
   this.confs   = confs;
+  this.fetchTry= false;
 }
 
 CF.prototype.testFetch = function () {
+  this.fetchTry = true;
+  return Q.reject('Fetch is not implemented');
 };
 
-CF.prototype.test = function () {
+CF.prototype.test = function (i) {
+  var self = this,
+      i    = typeof i === "undefined" ? 0 : i,
+      testFile;
+
+  testFile = self.problem + '/test' + i;
+
+  return self.cacher.exists(testFile)
+    .then(function () {
+      console.log(123);
+    })
+    .catch(function () {
+      if (self.fetchTry) {
+        throw 'Something went wrong while fetching..';
+      }
+
+      return self.testFetch()
+                 .then(function () {
+                   return self.test();
+                 })
+                 .catch(function (e) { throw e; })
+    });
 };
 
 CF.prototype.post = function () {
